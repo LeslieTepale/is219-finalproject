ce17549b8637eae5c31fff51021fb5617f370c77
LeslieTepale <77855205+LeslieTepale@users.noreply.github.com>
2022-05-12 14:58:40 -0400

    test case to check dashboard access

---
+++ tests/dashboard_access.py
@@ -0,0 +1,9 @@
+def test_dashboard_access(client):
+    """This makes the index page"""
+    response = client.get("/dashboard")
+    assert response.status_code == 302
+    response = client.get("/register")
+    assert response.status_code == 200
+    response = client.get("/login")
+    assert response.status_code == 200
+
