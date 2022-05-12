79545d54a96537d76d32dc0e3ad7dd24f8fa0ae3
LeslieTepale <77855205+LeslieTepale@users.noreply.github.com>
2022-05-11 22:57:14 -0400

    added javascript to registration page for validation

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 """A simple flask web app"""
 import os
+import flask_login
 from flask import Flask
+from flask_bootstrap import Bootstrap5
+from flask_wtf.csrf import CSRFProtect
 from app.cli import create_database
 from app.db import db
 from app.db.models import User
-
+from app.simple_pages import simple_pages
 
 def page_not_found(e):
     return render_template("404.html"), 404
@@ -14,7 +17,9 @@ def create_app():
     app = Flask(__name__)
     app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
 
+    bootstrap = Bootstrap5(app)
     app.register_error_handler(404, page_not_found)
+    app.register_blueprint(simple_pages)
     db_dir = "database/db.sqlite"
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
