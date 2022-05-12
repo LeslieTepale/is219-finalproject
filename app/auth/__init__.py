daa71df08f76e394dfbd4df474b676d7654d62bf
LeslieTepale <77855205+LeslieTepale@users.noreply.github.com>
2022-05-11 23:43:07 -0400

    successfully loaded index page

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -2,26 +2,25 @@ from flask import Blueprint, render_template, redirect, url_for, flash,current_a
 from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash
 
-from app.auth.forms import loginForm, registrationForm
+from app.auth.forms import login_form, register_form
 from app.db import db
 from app.db.models import User
+
 
 auth = Blueprint('auth', __name__, template_folder='templates')
 
 
 @auth.route('/register', methods=['POST', 'GET'])
 def register():
-    form = registrationForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('auth.dashboard'))
+    form = register_form()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is None:
             user = User(email=form.email.data, password=generate_password_hash(form.password.data))
             db.session.add(user)
             db.session.commit()
-            if user.id == 1:
-                user.is_admin = 1
-                db.session.add(user)
-                db.session.commit()
             flash('Congratulations, you are now a registered user!', "success")
             return redirect(url_for('auth.login'), 302)
         else:
@@ -31,7 +30,7 @@ def register():
 
 @auth.route('/login', methods=['POST', 'GET'])
 def login():
-    form = loginForm()
+    form = login_form()
     if current_user.is_authenticated:
         return redirect(url_for('auth.dashboard'))
     if form.validate_on_submit():
@@ -58,12 +57,6 @@ def logout():
     db.session.commit()
     logout_user()
     return redirect(url_for('auth.login'))
-
-
-
-
-
-
 
 
 
