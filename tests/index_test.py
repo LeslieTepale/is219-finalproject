b14ab909aaf129088e131e36d91adc5b7327b107
LeslieTepale <77855205+LeslieTepale@users.noreply.github.com>
2022-05-12 04:26:01 -0400

    first test case created and passed

---
+++ tests/index_test.py
@@ -0,0 +1,7 @@
+def test_request_main_menu_links(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b'href="/"' in response.data
+    assert b'href="/login"' in response.data
+    assert b'href="/register"' in response.data
