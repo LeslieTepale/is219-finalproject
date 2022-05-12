8ee1fae93dc84c7578a2bc9ddbfad7e97e4abe12
LeslieTepale <77855205+LeslieTepale@users.noreply.github.com>
2022-05-12 19:51:25 -0400

    new test cases added and passed

--- tests/index_test.py
+++ tests/index_test.py
@@ -5,3 +5,13 @@ def test_request_main_menu_links(client):
     assert b'href="/"' in response.data
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
+
+def test_open_login(client):
+    response = client.get("/login")
+    assert response.status_code == 200
+    assert b"login" in response.data
+
+def test_open_register(client):
+    response = client.get("/register")
+    assert response.status_code == 200
+    assert b"register" in response.data
